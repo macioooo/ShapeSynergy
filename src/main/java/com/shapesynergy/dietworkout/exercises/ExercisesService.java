16861e3100058d9a05d740e62a2bf6c47b2813c5
Maciej Szuwarowski <m9szuwarowski@interia.pl>
2024-02-20 00:04:26 +0100

    refactored code

--- src/main/java/com/shapesynergy/dietworkout/exercises/ExercisesService.java
+++ src/main/java/com/shapesynergy/dietworkout/exercises/ExercisesService.java
@@ -3,16 +3,12 @@ package com.shapesynergy.dietworkout.exercises;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ExercisesService {
@@ -20,9 +16,7 @@ public class ExercisesService {
     private final RestTemplate restTemplate;
     private final HttpHeaders headers;
     private final String URL = "https://api.api-ninjas.com/v1/exercises";
-    private int offset = 0;
-    private int offsetTemp = 0;
-    private String exerciseNames = "";
+    private final int LIMIT = 10; // Limit wyników do pobrania w jednym zapytaniu
 
     public ExercisesService(@Value("${api.keyExercises}") String apiKey) {
         this.restTemplate = new RestTemplate();
@@ -31,43 +25,31 @@ public class ExercisesService {
         this.headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
     }
 
-    //Get exercises by muscle (and optionally by name)
-    public String searchForExercise(String muscle, String exercise) throws JsonProcessingException {
-        // Początkowe wyszukiwanie ćwiczeń
+    private ResponseEntity<String> fetchExercisesFromApi(String muscle, String exercise, int offset) {
         HttpEntity<String> entity = new HttpEntity<>(headers);
         String url_with_params = URL + "?muscle=" + muscle + "&name=" + exercise + "&offset=" + offset;
-        ResponseEntity<String> response = restTemplate.exchange(url_with_params, HttpMethod.GET, entity, String.class);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return fetchAllExercises(response, muscle, exercise); // Przekazujemy muscle i exercise do metody fetchAllExercises
-        } else {
-            return "Failed to fetch exercise names. Status: " + response.getStatusCode();
-        }
+        return restTemplate.exchange(url_with_params, HttpMethod.GET, entity, String.class);
     }
 
-    private String fetchAllExercises(ResponseEntity<String> response, String muscle, String exercise) throws JsonProcessingException {
-        // Pobranie wszystkich ćwiczeń z odpowiedzi
-        StringBuilder exercisesBuilder = new StringBuilder();
-        int limit = 10; // Liczba wyników do pobrania w jednym zapytaniu
+    public String searchForExercise(String muscle, String exercise) throws JsonProcessingException {
         int offset = 0;
+        StringBuilder exercisesBuilder = new StringBuilder();
 
         while (true) {
-            JsonNode root = objectMapper.readTree(response.getBody());
-            if (root.size() == 0) {
-                // Nie ma więcej wyników, więc przerywamy pętlę
-                break;
+            ResponseEntity<String> response = fetchExercisesFromApi(muscle, exercise, offset);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                JsonNode root = objectMapper.readTree(response.getBody());
+                if (root.size() == 0) {
+                    break; // Przerwanie pętli, gdy nie ma więcej wyników
+                }
+                for (JsonNode exerciseNode : root) {
+                    String exerciseName = exerciseNode.get("name").asText();
+                    exercisesBuilder.append(exerciseName).append("<br>");
+                }
+                offset += LIMIT; // Aktualizacja offsetu
+            } else {
+                return "Failed to fetch exercise names. Status: " + response.getStatusCode();
             }
-            for (JsonNode exerciseNode : root) {
-                String exerciseName = exerciseNode.get("name").asText();
-                exercisesBuilder.append(exerciseName).append("<br>"); // Append each exercise name with system-dependent line separator
-            }
-            // Przesuwamy offset, aby pobrać następne wyniki
-            offset += limit + 1;
-
-            // Wykonujemy kolejne zapytanie
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            String url_with_params = URL + "?muscle=" + muscle + "&name=" + exercise + "&offset=" + offset;
-            response = restTemplate.exchange(url_with_params, HttpMethod.GET, entity, String.class);
         }
 
         return exercisesBuilder.toString();
